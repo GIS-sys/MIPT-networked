95416a97aa7de4da243b233f3a4096000b881ed1
GISwarm <gordeiegorov@gmail.com>
2025-05-19 12:44:31 +0300

    working double sided connection

--- w1/client.cpp
+++ w1/client.cpp
@@ -1,11 +1,16 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <netdb.h>
-#include <cstring>
+#include <arpa/inet.h>
 #include <cstdio>
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <string>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "socket_tools.h"
 
 
@@ -75,8 +80,9 @@ void Client::reset() {
 }
 
 
-int main(int argc, const char **argv)
-{
+
+
+int main() {
     Client client;
 
     client.connect("localhost", 2025);
@@ -85,9 +91,69 @@ int main(int argc, const char **argv)
         return 1;
     }
 
-    client.start_listening();
-    client.start_reading();
+    //client.start_listening();
+    //client.start_reading();
 
-    while (client.is_connected()) {}
+    //while (client.is_connected()) {}
+    //return 0;
+
+
+    // Resolve server address
+    addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if (getaddrinfo("localhost", "2025", &hints, &res) != 0) {
+        printf("Failed to resolve server address\n");
+        return 1;
+    }
+
+    sockaddr_in serverSockAddr;
+    memcpy(&serverSockAddr, res->ai_addr, res->ai_addrlen);
+    freeaddrinfo(res);
+
+    // Create client socket
+    int sfd = create_dgram_socket(nullptr, "0", nullptr); // Bind to any available port
+    if (sfd == -1) {
+        printf("Failed to create client socket\n");
+        return 1;
+    }
+
+    const char *pingMsg = "ping";
+    printf("Sending 'ping' to server...\n");
+    sendto(sfd, pingMsg, strlen(pingMsg), 0, (sockaddr*)&serverSockAddr, sizeof(serverSockAddr));
+
+    while (true) {
+        fd_set readFds;
+        FD_ZERO(&readFds);
+        FD_SET(sfd, &readFds);
+
+        timeval tv = {1, 0}; // 1 second timeout
+        int ready = select(sfd + 1, &readFds, nullptr, nullptr, &tv);
+
+        if (ready < 0) {
+            perror("select error");
+            break;
+        } else if (ready == 0) {
+            // Timeout: resend ping
+            printf("No response, resending 'ping'...\n");
+            sendto(sfd, pingMsg, strlen(pingMsg), 0, (sockaddr*)&serverSockAddr, sizeof(serverSockAddr));
+        } else {
+            char buffer[1024];
+            sockaddr_in responderAddr;
+            socklen_t addrLen = sizeof(responderAddr);
+            ssize_t numBytes = recvfrom(sfd, buffer, sizeof(buffer), 0, (sockaddr*)&responderAddr, &addrLen);
+
+            if (numBytes > 0) {
+                buffer[numBytes] = '\0';
+                printf("Received '%s' from server. Sending 'ping' again...\n", buffer);
+                sleep(1);
+                sendto(sfd, pingMsg, strlen(pingMsg), 0, (sockaddr*)&responderAddr, addrLen);
+            }
+        }
+    }
+
+    close(sfd);
     return 0;
 }
--- w1/server.cpp
+++ w1/server.cpp
@@ -1,50 +1,56 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/select.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <cstring>
 #include <cstdio>
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "socket_tools.h"
 
-int main(int argc, const char **argv)
-{
-  const char *port = "2025";
-
-  int sfd = create_dgram_socket(nullptr, port, nullptr);
-
-  if (sfd == -1)
-  {
-    printf("cannot create socket\n");
-    return 1;
-  }
-  printf("listening!\n");
-
-  while (true)
-  {
-    fd_set readSet;
-    FD_ZERO(&readSet);
-    FD_SET(sfd, &readSet);
-
-    timeval timeout = { 0, 100000 }; // 100 ms
-    select(sfd + 1, &readSet, NULL, NULL, &timeout);
 
 
-    if (FD_ISSET(sfd, &readSet))
-    {
-      constexpr size_t buf_size = 1000;
-      static char buffer[buf_size];
-      memset(buffer, 0, buf_size);
+int main() {
+    const char *port = "2025";
+    int sfd = create_dgram_socket(nullptr, port, nullptr);
 
-      sockaddr_in sin;
-      socklen_t slen = sizeof(sockaddr_in);
-      ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, (sockaddr*)&sin, &slen);
-      if (numBytes > 0)
-      {
-        printf("(%s:%d) %s\n", inet_ntoa(sin.sin_addr), sin.sin_port, buffer); // assume that buffer is a string
-      }
+    if (sfd == -1) {
+        printf("Cannot create socket\n");
+        return 1;
     }
-  }
-  return 0;
+    printf("Listening on port %s!\n", port);
+
+    while (true) {
+        fd_set readSet;
+        FD_ZERO(&readSet);
+        FD_SET(sfd, &readSet);
+
+        timeval timeout = {0, 100000}; // 100 ms
+        select(sfd + 1, &readSet, NULL, NULL, &timeout);
+
+        if (FD_ISSET(sfd, &readSet)) {
+            char buffer[1000];
+            sockaddr_in clientAddr;
+            socklen_t clientAddrLen = sizeof(clientAddr);
+            ssize_t numBytes = recvfrom(sfd, buffer, sizeof(buffer)-1, 0, (sockaddr*)&clientAddr, &clientAddrLen);
+
+            if (numBytes > 0) {
+                buffer[numBytes] = '\0';
+                printf("Received from %s:%d: %s\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), buffer);
+
+                // Send "pong" response
+                const char *response = "pong";
+                sendto(sfd, response, strlen(response), 0, (sockaddr*)&clientAddr, clientAddrLen);
+            }
+        }
+    }
+
+    close(sfd);
+    return 0;
 }
+
